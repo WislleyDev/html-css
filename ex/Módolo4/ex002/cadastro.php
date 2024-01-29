2abcb89fd5009503232c5771b8e80127b4650b20
Wislley marques vieira dos santos <w52993000@gmail.com>
2024-01-29 14:29:04 -0300

    ?

---
+++ ex/Módolo4/ex002/cadastro.php
@@ -0,0 +1,3 @@
+<h1>kkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkk é php e não html</h1>
+Os arquivos foram eviados!
+Aprenda PHP para saber o que fazer com eles.
